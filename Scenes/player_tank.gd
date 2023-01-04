1e7cee3e92d16f194e81bb71fc343aa89b223da6
RizLysander <rizlysander@gmail.com>
2023-01-04 10:10:51 -0600

    rl:Add Rotate around origin, added test scene for tanks

--- Scenes/player_tank.gd
+++ Scenes/player_tank.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+#Rotation Speed in Radians
+const ROTATION_SPEED = 0.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -28,4 +31,13 @@ func _physics_process(delta):
 	var left_input_dir = Input.get_vector("left_hover_bank_left", "left_hover_bank_right", "left_hover_bank_forward", "left_hover_bank_backward")
 	var right_input_dir = Input.get_vector("right_hover_bank_left", "right_hover_bank_right", "right_hover_bank_forward","right_hover_bank_backward")
 	
+	if(left_input_dir.length() == 0 && right_input_dir.length() == 0):
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+#	elif((left_input_dir.length() == 0 || right_input_dir.length() == 0) && !(left_input_dir.length() != 0 && right_input_dir.length() != 0)):
+	else:
+		var rotation = left_input_dir.y + (right_input_dir.y * -1.0)
+		rotate_object_local(Vector3(Vector3.UP), ROTATION_SPEED * rotation)
+		transform = transform.orthonormalized()
+		
 	move_and_slide()
